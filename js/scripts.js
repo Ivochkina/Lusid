8945764fcbf44caa45d01decbe60f7bd13115068
Ivochkina <romina23082008@gmail.com>
2020-08-10 18:50:54 +0200

    finish

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:0,
-        nav:true,
+        nav:false,
         navText: [ '', ' ' ],
 
         responsive:{
